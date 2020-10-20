c5bd8e0ba20f41594815a6c3c423c5acfd6407d2
Pedro Sousa <pedrosousa2708@gmail.com>
2020-10-20 16:08:22 -0300

    Fix if the default value for the menu function is null the option 0 is not shown

--- src/Utils/Menu.java
+++ src/Utils/Menu.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Menu {
     public static int createMenu(String[] items, String defaultValue) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("0 - " + defaultValue);
+        if (defaultValue != null) {
+            System.out.println("0 - " + defaultValue);
+        }
         while (true) {
             for (int i = 0; i < items.length; i++) {
                 System.out.println(i + 1 + " - " + items[i]);
