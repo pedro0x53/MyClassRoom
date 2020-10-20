81afdd722354d41590c0152c9ce48a92fc54bf57
Pedro Sousa <pedrosousa2708@gmail.com>
2020-10-20 16:12:57 -0300

    Menu default value removed

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import Utils.Menu;
 public class Main {
     public static void main(String[] args) {
         AuthenticationController ac = new AuthenticationController();
-        String[] items = {"Login", "Cadastro"};
-        int res = Menu.createMenu(items, "Sair");
+        String[] items = {"Sair", "Login", "Cadastro"};
+        int res = Menu.createMenu(items);
         if (res == 1) {
             ac.login();
         } else if (res == 2) {
--- src/Utils/Menu.java
+++ src/Utils/Menu.java
@@ -3,19 +3,16 @@ package Utils;
 import java.util.Scanner;
 
 public class Menu {
-    public static int createMenu(String[] items, String defaultValue) {
+    public static int createMenu(String[] items) {
         Scanner keyboard = new Scanner(System.in);
-        if (defaultValue != null) {
-            System.out.println("0 - " + defaultValue);
-        }
         while (true) {
             for (int i = 0; i < items.length; i++) {
                 System.out.println(i + 1 + " - " + items[i]);
             }
             System.out.print("Escolha uma das opções acima: ");
-            int req = keyboard.nextInt();
+            int req = keyboard.nextInt() - 1;
             System.out.println();
-            if (req >= 0 && req <= items.length) {
+            if (req >= 0 && req < items.length) {
                 return req;
             } else {
                 System.out.println("Opção inválida.");
