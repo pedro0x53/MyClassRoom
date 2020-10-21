411a919332eb762cc911496e8a7b286cc34ca09f
Pedro Sousa <pedrosousa2708@gmail.com>
2020-10-21 01:17:22 -0300

    Main controller reset

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,13 @@ import Utils.Menu;
 
 public class Main {
     public static void main(String[] args) {
-//        AuthenticationController ac = new AuthenticationController();
-//        String[] items = {"Sair", "Login", "Cadastro"};
-//        int res = Menu.createMenu(items);
-//        if (res == 1) {
-//            ac.login();
-//        } else if (res == 2) {
-//            ac.signUp();
-//        }
-
-        AssignmentController ac = new AssignmentController();
-        ac.view();
+        AuthenticationController ac = new AuthenticationController();
+        String[] items = {"Sair", "Login", "Cadastro"};
+        int res = Menu.createMenu(items);
+        if (res == 1) {
+            ac.login();
+        } else if (res == 2) {
+            ac.signUp();
+        }
     }
 }
