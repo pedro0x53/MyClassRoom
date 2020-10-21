b92001cdbd6965980cbe264e825df8c2ea06d170
Brena Amorim <brena.amorim@live.com>
2020-10-21 18:20:11 -0300

    Class View and Controller

---
+++ src/View/Class.java
@@ -0,0 +1,38 @@
+package View;
+
+import Model.Classes;
+import Utils.FormFields;
+import Utils.Menu;
+
+public class Class {
+    private FormFields ff = new FormFields();
+
+    public String createClass() {
+        String nameClass;
+        System.out.println("----- Criar Turma -----");
+        nameClass = ff.askForTextField("nome da turma", "Nome da turma: ", true);
+        return nameClass;
+    }
+
+    public String enrollClass() {
+        String code;
+        System.out.println("----- Participar da turma -----");
+        code = ff.askForTextField("código", "Código da turma: ", true);
+        return code;
+    }
+
+    public int showClasses( Classes[] classes ) {
+        System.out.println("----- Turmas -----");
+        String[] messages = new String[100];
+
+        for (int i = 0; i < classes.length; i++) {
+            String message = "" + classes[i].name + "";
+            messages[i] = message;
+        }
+
+        int answer = Menu.createMenu(messages);
+
+        return answer;
+    }
+
+}
